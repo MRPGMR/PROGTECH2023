82b5195ee8a4841e24c1f2ed6017857b2c34f142
MRPGMR <oroszp26@gmail.com>
2023-10-25 20:14:17 +0200

    1.2

--- wumpus/src/main/java/org/example/Main.java
+++ wumpus/src/main/java/org/example/Main.java
@@ -12,31 +12,35 @@ public class Main {
         String jatekosNev = scanner.nextLine();
 
         while (true) {
-            System.out.println("Üdvözöllek a Wumplusz játékban!"+jatekosNev);
+            System.out.println("Üdvözöllek a Wumplusz játékban "+jatekosNev +"!");
             System.out.println("\nFőmenü:");
             System.out.println("1. Pályaszerkesztés");
-            System.out.println("2. Mentés");
-            System.out.println("3. Betöltés");
-            System.out.println("4. Játszás");
-            System.out.println("5. Kilépés");
+            System.out.println("2. Fájl beolvasás");
+            System.out.println("3. Adatbázisból betöltés");
+            System.out.println("3. Adatbázisba mentés");
+            System.out.println("5. Játszás");
+            System.out.println("6. Kilépés");
 
             System.out.print("Kérem, válassz egy opciót (1-5): ");
             int valasztas = scanner.nextInt();
 
             switch (valasztas) {
                 case 1:
-                    editMap();
+                    Szerkesztes();
                     break;
                 case 2:
-                    saveGame();
+                    Beolvasas();
                     break;
                 case 3:
-                    loadGame();
+                    Betoltes();
                     break;
                 case 4:
-                    playGame();
+                    Mentes();
                     break;
                 case 5:
+                    Jatszas();
+                    break;
+                case 6:
                     System.out.println("Viszontlátás!");
                     scanner.close();
                     System.exit(0);
@@ -47,22 +51,26 @@ public class Main {
         }
     }
 
-    public static void editMap() {
+    public static void Szerkesztes() {
+        // Pályaszerkesztés
+        System.out.println("Pályaszerkesztés opció kiválasztva.");
+    }
+    public static void Beolvasas() {
         // Pályaszerkesztés
         System.out.println("Pályaszerkesztés opció kiválasztva.");
     }
 
-    public static void saveGame() {
+    public static void Mentes() {
         // Mentés
         System.out.println("Mentés opció kiválasztva.");
     }
 
-    public static void loadGame() {
+    public static void Betoltes() {
         // Betöltés
         System.out.println("Betöltés opció kiválasztva.");
     }
 
-    public static void playGame() {
+    public static void Jatszas() {
         // Játékmenet
         System.out.println("Játszás opció kiválasztva.");
     }
